89026c549a73c8cdb5705d1d24710e9ed25a9d4d
YJ <jyjin1206@gmail.com>
2026-03-10 12:28:00 +0900

    Fix some isseus

--- datasets/custom.py
+++ datasets/custom.py
@@ -17,52 +17,72 @@ def get_transforms(img_size=None):
 class CustomDataset(data.Dataset):
     def __init__(self, configs):
         super().__init__()
-        self.data_path = configs['dataset']['root']
-        self.img_size = configs['dataset']['img_size']
-        self.in_channels = configs['dataset']['in_channels']
+        dataset_cfg = configs.get('dataset', {})
+
+        self.data_path = dataset_cfg['root']
+        self.img_size = dataset_cfg['img_size']
+        self.in_channels = dataset_cfg['in_channels']
+
+        if not os.path.isdir(self.data_path):
+            raise FileNotFoundError(f"Custom dataset root does not exist: {self.data_path}")
+
         self.image_paths = get_image_paths(self.data_path)
         self.transform = get_transforms(self.img_size)
 
         self.use_text = False
         self.texts = None
 
-        if configs['task'] == 'latent_diffusion':
-            text_cfg = configs['dataset'].get('text', None)
+        if configs.get('task') == 'latent_diffusion':
+            self._enable_text_conditioning(dataset_cfg)
 
-            if isinstance(text_cfg, dict) and 'root' in text_cfg:
-                text_root = text_cfg['root']
-                txt_files = glob.glob(os.path.join(text_root, '*.txt'))
-                txt_map = {
-                    os.path.splitext(os.path.basename(path))[0]: path
-                    for path in txt_files
-                }
+    def _enable_text_conditioning(self, dataset_cfg):
+        text_cfg = dataset_cfg.get('text', None)
+        if not isinstance(text_cfg, dict) or 'root' not in text_cfg:
+            raise ValueError("For latent_diffusion with custom dataset, dataset.text.root is required.")
 
-                matched_image_paths = []
-                matched_texts = []
+        text_root = text_cfg['root']
+        if not os.path.isdir(text_root):
+            raise FileNotFoundError(f"Caption root does not exist: {text_root}")
 
-                for image_path in self.image_paths:
-                    image_name = os.path.splitext(os.path.basename(image_path))[0]
-                    text_path = txt_map.get(image_name, None)
+        txt_map = self._build_text_map(text_root)
+        matched_image_paths, matched_texts = self._match_images_and_texts(txt_map)
 
-                    if text_path is None:
-                        continue
+        self.image_paths = matched_image_paths
+        self.texts = matched_texts
+        self.use_text = True
 
-                    with open(text_path, 'r', encoding='utf-8') as f:
-                        text = f.read().strip()
+        if len(self.image_paths) == 0:
+            raise RuntimeError(
+                f'No matched image-text pairs found.\n'
+                f'image root: {self.data_path}\n'
+                f'text root: {text_root}'
+            )
 
-                    matched_image_paths.append(image_path)
-                    matched_texts.append(text)
+    def _build_text_map(self, text_root):
+        txt_files = glob.glob(os.path.join(text_root, '*.txt'))
+        return {
+            os.path.splitext(os.path.basename(path))[0]: path
+            for path in txt_files
+        }
 
-                self.image_paths = matched_image_paths
-                self.texts = matched_texts
-                self.use_text = True
+    def _match_images_and_texts(self, txt_map):
+        matched_image_paths = []
+        matched_texts = []
 
-                if len(self.image_paths) == 0:
-                    raise RuntimeError(
-                        f'No matched image-text pairs found.\n'
-                        f'image root: {self.data_path}\n'
-                        f'text root: {text_root}'
-                    )
+        for image_path in self.image_paths:
+            image_name = os.path.splitext(os.path.basename(image_path))[0]
+            text_path = txt_map.get(image_name)
+
+            if text_path is None:
+                continue
+
+            with open(text_path, 'r', encoding='utf-8') as f:
+                text = f.read().strip()
+
+            matched_image_paths.append(image_path)
+            matched_texts.append(text)
+
+        return matched_image_paths, matched_texts
 
     def __len__(self):
         return len(self.image_paths)
